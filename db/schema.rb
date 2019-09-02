9a6fdd313ec901b44731cc9eff6b62287bc48d69
AroRk226 <arorakotonindrina12@gmail.com>
2019-09-02 16:24:01 +0300

    Dev

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_09_02_101923) do
+ActiveRecord::Schema.define(version: 2019_09_02_100330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,9 +22,7 @@ ActiveRecord::Schema.define(version: 2019_09_02_101923) do
     t.string "image_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-
   end
-
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -37,7 +34,6 @@ ActiveRecord::Schema.define(version: 2019_09_02_101923) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
   end
 
 end
