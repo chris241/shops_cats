b2c8e74245c508b5d4e09b85d53b4bb8a5f51721
chris241 <chrisleon325@gmail.com>
2019-09-02 15:40:49 +0300

    master

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
 
 	validates :title, presence: true, length: { in: 5..10 }
-	validates :description, presence: true, length: { in: 10..200 }
+	validates :description, presence: true, length: { in: 5..200 }
 	validates :price, presence: true
 	URL_REGEXP = /A^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$Z/ix
 	validates :image_url, presence: true, format: { with: URL_REGEXP, message: 'You provided invalid URL'}
