8d34dbfff26e6a68ebcdd5d2163461583c52d6cc
tsiry <YOUR@EMAIL.com>
2019-09-03 16:41:25 +0300

    create index

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2019_09_03_073328) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,14 +31,6 @@ ActiveRecord::Schema.define(version: 2019_09_03_073328) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "joins", force: :cascade do |t|
-    t.bigint "item_id"
-    t.bigint "cart_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["cart_id"], name: "index_joins_on_cart_id"
-    t.index ["item_id"], name: "index_joins_on_item_id"
-
   create_table "join_order_items", force: :cascade do |t|
     t.bigint "order_id"
     t.bigint "item_id"
@@ -49,12 +40,20 @@ ActiveRecord::Schema.define(version: 2019_09_03_073328) do
     t.index ["order_id"], name: "index_join_order_items_on_order_id"
   end
 
+  create_table "joins", force: :cascade do |t|
+    t.bigint "item_id"
+    t.bigint "cart_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cart_id"], name: "index_joins_on_cart_id"
+    t.index ["item_id"], name: "index_joins_on_item_id"
+  end
+
   create_table "orders", force: :cascade do |t|
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_orders_on_user_id"
-
   end
 
   create_table "users", force: :cascade do |t|
