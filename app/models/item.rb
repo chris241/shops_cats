0f9e7ed40f2227e660264f5fbf54a2c55a847346
Nam-9 <naninambinina@gmail.com>
2019-09-03 11:02:00 +0300

    validate

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,11 @@
 class Item < ApplicationRecord
 
-	validates :title, presence: true, length: { in: 5..10 }
-	validates :description, presence: true, length: { in: 10..200 }
+	validates :title, presence: true, length: { in: 2..10 }
+	validates :description, presence: true, length: { in: 2..200 }
 	validates :price, presence: true
-	validates :image_url, presence: true
+	URL_REGEXP = /\A^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$\z/ix
+	validates :image_url, presence: true, format: { with: URL_REGEXP, message: 'You provided invalid URL'}
+	
 
 
 
