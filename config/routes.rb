97c918a78f3751f40e84461c1bfedc55197e5218
Nam-9 <naninambinina@gmail.com>
2019-09-03 09:21:32 +0300

    seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-	  root 'home#index'
-      devise_for :users
-=======
+
 resources :items, only: [:show, :index]
   root 'items#index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
->>>>>>> eaa26490a6e2d91dc7a1182756f4528a04a5d77d
 end
