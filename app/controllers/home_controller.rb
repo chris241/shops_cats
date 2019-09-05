ae39b592bf191f38661bc7de82ceb867d761c545
AroRk226 <arorakotonindrina12@gmail.com>
2019-09-05 12:46:21 +0300

    Mise à jour

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,23 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def profil
+    if user_signed_in?
+      @cart = Cart.find_by(user_id: current_user.id)
+      if @cart != nil
+        associations = JoinItemCart.where(cart_id: @cart.id)
+        @items = []
+        associations.each do |a|
+          @items.push(Item.find(a.item_id))
+        end
+      else
+        @cart = Cart.new
+        current_user.cart = @cart
+        @cart.save
+        @items = []
+	  end
+	end
+  end
+
 end
