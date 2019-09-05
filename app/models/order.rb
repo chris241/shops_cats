003fe9a0f1825dfeaf272994c9827259c2a47329
AroRk226 <arorakotonindrina12@gmail.com>
2019-09-05 08:36:44 +0300

    Mise à jour

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,10 @@ class Order < ApplicationRecord
 	belongs_to :user
 	has_many :join_order_items
 	has_many :items, through: :join_order_items
+
+	after_create :order_email_send
+
+	def order_email_send
+		UserMailer.order_email(self).deliver_now
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
 
 resources :carts, only:[:create, :show]
 resources :items, only: [:show, :index]
+
   root 'items#index'
+  get '/profil', to: 'home#index'
+
   devise_for :users
+
  devise_scope :user do
      get '/users/sign_out'=> 'devise/sessions#destroy'
  end
