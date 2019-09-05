af6ea32be109ec4eef8146e6a1ec0fbc4a17c6cc
tsiry <YOUR@EMAIL.com>
2019-09-05 08:06:33 +0300

    remake front page

--- app/javascript/packs/test.js
+++
@@ -1,8 +0,0 @@
-// Modification 4: populate image
-var imagesArray = ["https://img.icons8.com/color/480/000000/html-5.png", "https://img.icons8.com/color/480/000000/css3.png", "https://img.icons8.com/color/480/000000/javascript.png", "https://img.icons8.com/color/480/000000/ruby-programming-language.png", "https://img.icons8.com/color/480/000000/bootstrap.png", "https://img.icons8.com/color/480/000000/github.png", "http://jeudisdulibre.be/wp-content/uploads/2014/02/Ruby_on_Rails-logo.png", "https://avatars2.githubusercontent.com/u/25484553?s=200&v=4", "https://img.icons8.com/color/480/000000/heroku.png"];
-    
-    function populateImage(){
-        for (let i = 0 ; i < imagesArray.length ; i++){
-            document.getElementsByClassName('card-img-top')[i].src = imagesArray[i];
-        }
-    }
