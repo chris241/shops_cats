bba65b215d74fc2533bfe589bb4b79e211008338
Nam-9 <naninambinina@gmail.com>
2019-09-05 17:45:13 +0300

    stripe

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,8 +1,12 @@
 class CartsController < ApplicationController
 	before_action :authenticate_user!
+
+
+
   def create
-    @u=current_user.id
-    @i=session[:item_id]
+     
+     @u=current_user.id
+     @i=session[:item_id]
   if current_user.cart == nil
       @cart = Cart.create(user_id: @u)
     end
@@ -13,14 +17,15 @@ class CartsController < ApplicationController
      puts "Try again"
      end
  end
-
+ 
  def show
+  @item = Item.find(session[:item_id])
    @cart = Cart.find(params[:id])
    @tab= @cart.items
    allitems = current_user.cart.items
    c = 0
    @nbr = allitems.length
-   session[:nbr] = @nbr
+   session[:nbr] = @nbr 
    allitems.each do |item|
        c += item.price
        @count = c
